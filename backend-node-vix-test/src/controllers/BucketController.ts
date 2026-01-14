70202fc0599355a86fb74ce4ee3d070ea3f933f7
Thálisson Gonçalves <thalissongdev@gmail.com>
2026-01-14 03:15:25 -0300

    fix: adjust request parameter typing in BucketController

--- backend-node-vix-test/src/controllers/BucketController.ts
+++ backend-node-vix-test/src/controllers/BucketController.ts
@@ -12,12 +12,28 @@ export class BucketController {
     res: Response,
   ) {
     const { objectName } = req.params;
+
+    // Verifica se é array e retorna com código e mensagem de erro caso for array
+    if (Array.isArray(objectName)) {
+    return res
+      .status(STATUS_CODE.BAD_REQUEST)
+      .json({ message: "Invalid object name" });
+    }
+
     const filePath = path.join(__dirname, "..", "..", "uploads", objectName);
     return res.sendFile(filePath);
   }
 
   async getFileByObjectName(req: CustomRequest<unknown>, res: Response) {
     const { objectName } = req.params;
+
+    // Verifica se é array e retorna com código e mensagem de erro caso for array
+    if (Array.isArray(objectName)) {
+    return res
+      .status(STATUS_CODE.BAD_REQUEST)
+      .json({ message: "Invalid object name" });
+    }
+
     const response = await this.bucketService.renewPresignedUrl(objectName);
     return res.status(STATUS_CODE.OK).json({ url: response });
   }
